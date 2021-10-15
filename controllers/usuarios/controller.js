6e88d6b2da2f07a0f05a848bca2118a6e9b176b4
Juan Esteban Montoya <90530464+juanemontoya@users.noreply.github.com>
2021-10-14 23:30:39 -0500

    Add files via upload
    Nuevoback con Auth0

---
+++ controllers/usuarios/controller.js
@@ -0,0 +1,99 @@
+
+import { getDB } from '../../db/db.js';
+import { ObjectId } from 'mongodb';
+
+
+//consulta el listado de los usuarios
+
+const consultaTodosUsuarios = async (callback)=>{
+
+    const connection = getDB();
+    await connection.collection('usuarios').find().limit(50).toArray(callback);
+      
+};
+
+
+// consulta un usuario especifico por id
+
+const consultaUsuario =async(id,callback) =>{
+
+  const connection = getDB();
+  await connection.collection('usuarios').findOne({ _id: new ObjectId(id)},callback);
+
+};
+
+
+
+// crea un nuevo Producto
+
+const crearUsuario = async (datosUsuario, callback)=>{
+
+  
+  console.log('Llaves: ', Object.keys(datosUsuario));
+
+      if (
+          Object.keys(datosUsuario).includes('usuario') &&
+          Object.keys(datosUsuario).includes('rol') &&
+          Object.keys(datosUsuario).includes('estado')
+        ) {
+          // implementar código para crear vehículo en la BD
+          const connection = getDB();
+          console.log(datosUsuario.usuario);
+          const ss =  { usuario: datosUsuario.usuario};
+          console.log(ss);
+
+          const resultado = await connection.collection('usuarios').findOne(ss);
+
+          if(resultado){
+
+            return callback(409)
+            
+          }else{
+
+            await connection.collection('usuarios').insertOne(datosUsuario, callback);
+
+          }         
+          
+        } else {
+          return { err: 'Error en consulta de datos', result: "Hola" };
+        }
+};
+
+
+//editar un usuario
+
+const editarUsuario =async (id,edicion, callback)=>{
+
+  
+  const filtroProducto = { _id: new ObjectId(id) };
+  console.log("aqui,",id);
+  console.log(filtroProducto);
+  const operacion = {
+    $set: edicion,
+  };
+  const connection = getDB();
+ 
+  await connection.collection('usuarios').findOneAndUpdate(filtroProducto,operacion, { upsert: true, returnOriginal: true },callback);
+
+};
+
+
+
+
+//elminar un usuario
+
+const eliminarUsuario = async (id, callback)=>{
+
+    const filtroUsuario = { _id: new ObjectId(id) };
+    
+    const connection = getDB();
+    await connection.collection('usuarios').deleteOne(filtroUsuario, callback);
+
+};
+
+
+
+export {consultaTodosUsuarios, crearUsuario, editarUsuario, eliminarUsuario, consultaUsuario};
+
+
+
--- server.js
+++ server.js
@@ -1,201 +1,55 @@
-// hacer el import de express tradicional
-// const express = require('express');
-// hacer el nuevo import
+//hacer el import express
+//const express = require('express');
+//hacer el nuevo tipo de import
 import Express from 'express';
-import { MongoClient, ObjectId } from 'mongodb';
 import Cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './db/db.js';
+import rutasProducto from './views/productos/rutas.js';
+import rutasUsuario from './views/usuarios/rutas.js';
+import rutasVenta from './views/ventas/rutas.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
-const stringConexion = 
-'mongodb+srv://admin:admin@proyecto5x5.at40i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const client = new MongoClient(stringConexion,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
-let baseDeDatos;
+dotenv.config({path: './.env'});
+
 
 const app = Express();
 
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://misiontic-prueba.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-autenticacion-prueba-mintic',
+issuer: 'https://misiontic-prueba.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+
+
+
+app.use(jwtCheck);
 app.use(Express.json());
 app.use(Cors());
-
-app.get('/productos', (req, res) => {
-  console.log('alguien hizo get en la ruta /productos');
-  baseDeDatos
-    .collection('productos')
-    .find()
-    .limit(50)
-    .toArray((err, result) => {
-      if (err) {
-        res.status(500).send('Error consultando los productos');
-      } else {
-        res.json(result);
-      }
-    });
-});
-
-app.post('/productos/nuevo', (req, res) => {
-  console.log(req);
-  const datosProducto = req.body;
-  console.log('llaves: ', Object.keys(datosProducto));
-  try {
-    if (
-      Object.keys(datosProducto).includes('codigoProducto') &&
-      Object.keys(datosProducto).includes('descripcionProducto') &&
-      Object.keys(datosProducto).includes('valorUnitario') &&
-      Object.keys(datosProducto).includes('estado')
-    ) {
-      // implementar código para crear vehículo en la BD
-      baseDeDatos.collection('productos').insertOne(datosProducto, (err, result) => {
-        if (err) {
-          console.error(err);
-          res.sendStatus(500);
-        } else {
-          console.log(result);
-          res.sendStatus(200);
-        }
-      });
-    } else {
-      res.sendStatus(500);
-    }
-  } catch {
-    res.sendStatus(500);
-  }
-});
-
-app.patch('/productos/actualizar', (req, res) => {
-  const edicion = req.body;
-  console.log(edicion);
-  const filtroProducto = { _id: new ObjectId(edicion.id) };
-  delete edicion.id;
-  const operacion = {
-    $set: edicion,
-  };
-  baseDeDatos
-    .collection('productos')
-    .findOneAndUpdate(
-      filtroProducto,
-      operacion,
-      { upsert: true, returnOriginal: true },
-      (err, result) => {
-        if (err) {
-          console.error('error actualizando el producto: ', err);
-          res.sendStatus(500);
-        } else {
-          console.log('actualizado con exito');
-          res.sendStatus(200);
-        }
-      }
-    );
-});
-
-app.delete('/productos/eliminar', (req, res) => {
-  const filtroProducto = { _id: new ObjectId(req.body.id) };
-  baseDeDatos.collection('productos').deleteOne(filtroProducto, (err, result) => {
-    if (err) {
-      console.error(err);
-      res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-});
-
-app.get('/usuarios', (req, res) => {
-  console.log('alguien hizo get en la ruta /usuarios');
-  baseDeDatos
-    .collection('usuarios')
-    .find()
-    .limit(50)
-    .toArray((err, result) => {
-      if (err) {
-        res.status(500).send('Error consultando los usurios');
-      } else {
-        res.json(result);
-      }
-    });
-});
-
-app.post('/usuarios/nuevo', (req, res) => {
-  console.log(req);
-  const datosUsuario= req.body;
-  console.log('llaves: ', Object.keys(datosUsuario));
-  try {
-    if (
-      Object.keys(datosUsuario).includes('usuario') &&
-      Object.keys(datosUsuario).includes('rol') &&
-      Object.keys(datosUsuario).includes('estado')
-    ) {
-      // implementar código para crear vehículo en la BD
-      baseDeDatos.collection('usuarios').insertOne(datosUsuario, (err, result) => {
-        if (err) {
-          console.error(err);
-          res.sendStatus(500);
-        } else {
-          console.log(result);
-          res.sendStatus(200);
-        }
-      });
-    } else {
-      res.sendStatus(500);
-    }
-  } catch {
-    res.sendStatus(500);
-  }
-});
-
-app.patch('/usuarios/actualizar', (req, res) => {
-  const edicion = req.body;
-  console.log(edicion);
-  const filtroUsuario = { _id: new ObjectId(edicion.id) };
-  delete edicion.id;
-  const operacion = {
-    $set: edicion,
-  };
-  baseDeDatos
-    .collection('usuarios')
-    .findOneAndUpdate(
-      filtroUsuario,
-      operacion,
-      { upsert: true, returnOriginal: true },
-      (err, result) => {
-        if (err) {
-          console.error('error actualizando el usuario: ', err);
-          res.sendStatus(500);
-        } else {
-          console.log('actualizado con usuario');
-          res.sendStatus(200);
-        }
-      }
-    );
-});
-
-app.delete('/usuarios/eliminar', (req, res) => {
-  const filtroUsuario = { _id: new ObjectId(req.body.id) };
-  baseDeDatos.collection('usuarios').deleteOne(filtroUsuario, (err, result) => {
-    if (err) {
-      console.error(err);
-      res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-});
+app.use(rutasProducto);
+app.use(rutasUsuario);
+app.use(rutasVenta);
 
 
-const main = () => {
-  client.connect((err, db) => {
-    if (err) {
-      console.error('Error conectando a la base de datos');
-      return 'error';
-    }
-    baseDeDatos = db.db('ModVentas');
-    console.log('baseDeDatos exitosa');
-    return app.listen(5000, () => {
-      console.log('escuchando puerto 5000');
-    });
-  });
+
+const main = ()=>{
+
+  return app.listen(process.env.PORT, ()=>{
+
+    console.log(`escuchando puerto ${process.env.PORT}`);
+
+  }); 
+
 };
 
-main();
+connectDB(main);
